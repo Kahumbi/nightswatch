070d2f6cc0d8669f8f85b28b18fdb6167eefb6d1
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-06-18 23:42:02 +0300

    Add a route for registering users

--- users/views.py
+++ users/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# Create your views here
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('hood-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
