36680fc8f52e0a4624de8f0c2d834b92807104ab
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-06-19 00:11:11 +0300

    Add crispy forms to style up the form

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'users.apps.UsersConfig',
     'hood.apps.HoodConfig',
     'django.contrib.admin',
@@ -119,7 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
